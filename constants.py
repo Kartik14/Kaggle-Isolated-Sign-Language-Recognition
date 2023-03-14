b6e9f87bcfa49e21d9c996969d97a30124b8b7ea
Kartik Khandelwal <kartikkhandelwal1998@gmail.com>
2023-03-14 11:40:04 +0900

    added num labels const

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 DATA_ROOT = "/home/kartik/Kaggle/Google_Isolated_Sign_Language_Recognition/data/asl-signs"
 OUTPUT_ROOT = "/home/kartik/Kaggle/Google_Isolated_Sign_Language_Recognition/trained_models"
 
+NUM_LABELS = 250
 ROWS_PER_FRAME = 543
 FACE_LANDMARKS_RANGE = (0, 468)
 LEFT_HAND_LANDMARKS_RANGE = (468, 489)
